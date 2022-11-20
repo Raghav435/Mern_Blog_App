fad85d9e7aedc61ca545c0367e48cb41c1dcdf8a
Raghav Shukla <shuklaraghav435@gmail.com>
2022-11-20 16:21:41 +0530

    app added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import PostsList from './components/PostsList';
+import PostDetails from './components/PostDetails';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer autoClose={3000} />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/posts" component={PostsList} />
+          <Route exact path="/posts/:id" component={PostDetails} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
