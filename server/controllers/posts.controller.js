7725521af46d1f4056f09df115b182868a16abd5
Raghav Shukla <shuklaraghav435@gmail.com>
2022-11-20 15:24:48 +0530

    update post added

--- server/controllers/posts.controller.js
+++ server/controllers/posts.controller.js
@@ -34,3 +34,16 @@ export const createPost = async (req, res) => {
     });
   }
 };
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(_id, post, { new: true });
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(409).json({
+      message: error.message,
+    });
+  }
+};
